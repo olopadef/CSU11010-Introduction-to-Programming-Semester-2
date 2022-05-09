7c537acaec5fed3b9f319db378ade6559a225c98
Faith Olopade <91458920+olopadef@users.noreply.github.com>
2022-05-09 11:17:46 +0100

    Add files via upload

---
+++ ETests/FileIOTest.java
@@ -0,0 +1,20 @@
+
+public class FileIOTest {
+
+	public static void main(String[] args) {
+		FileIO longestWord = new FileIO();
+		longestWord = findLongestWord();
+		System.out.println(longestWord);
+
+		String[] fileArray = fileToArray();
+		System.out.print(fileArray);
+
+	}
+public String toString(FileIO longestWord, FileIO textArray) {
+	String longestWord1 = longestWord1.findLongestWord();
+	
+	return null;
+	
+	
+}
+}
